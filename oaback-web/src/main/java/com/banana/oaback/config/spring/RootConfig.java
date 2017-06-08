3d748c8ca8f2084e733e7df20fe4e439f226ae30
francisyang <francisyang@192.168.0.173>
2017-06-08 23:37:46 +0800

    init a multi maven module project-oaback

---
+++ oaback-web/src/main/java/com/banana/oaback/config/spring/RootConfig.java
@@ -0,0 +1,14 @@
+package com.banana.oaback.config.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = { "com.banana.oaback" }, excludeFilters = {
+		@Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class) })
+public class RootConfig {
+
+}
---
+++ oaback-web/src/main/java/com/banana/oaback/config/spring/WebConfig.java
@@ -0,0 +1,31 @@
+package com.banana.oaback.config.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.banana.oaback.controller")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		// viewResolver.setContentType("text/html");
+		viewResolver.setPrefix("/WEB-INF/");
+		viewResolver.setSuffix(".html");
+		viewResolver.setExposeContextBeansAsAttributes(true);
+		return viewResolver;
+	}
+	
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+}
