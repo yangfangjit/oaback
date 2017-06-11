b4a79e1defc24326a0ae5705f9ee9fe2e09e03c9
francisyang <francisyang@192.168.0.173>
2017-06-11 22:09:48 +0800

    added a service unit test

---
+++ oaback-service/src/test/java/com/banana/oaback/service/EmployeeServiceTest.java
@@ -0,0 +1,41 @@
+package com.banana.oaback.service;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.banana.oaback.bean.Employee;
+
+public class EmployeeServiceTest extends BaseTest {
+	
+	private static final Logger logger = LoggerFactory.getLogger(EmployeeServiceTest.class);
+	
+	@Autowired
+	private EmployeeService employeeService;
+
+	@Test
+	@Rollback
+	@Transactional
+	public void testEmployeeService() {
+		String name = "yangfang";
+		short age = 26;
+		Employee employee = new Employee(name, age);
+		
+		employeeService.insertEmployee(employee);
+		
+		List<Employee> list = employeeService.queryAll(0, 100);
+		
+		logger.info(list.get(0).toString());
+		
+		assertEquals(name, list.get(0).getName());
+		assertEquals(age, list.get(0).getAge());
+	}
+
+}
