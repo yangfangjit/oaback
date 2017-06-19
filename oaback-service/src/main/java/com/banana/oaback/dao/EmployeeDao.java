09e2b9ed7930a236fdabde806a2b34c2b0dd3657
francisyang <francisyang@192.168.0.173>
2017-06-19 22:45:35 +0800

    added some comments

--- oaback-service/src/main/java/com/banana/oaback/dao/EmployeeDao.java
+++ oaback-service/src/main/java/com/banana/oaback/dao/EmployeeDao.java
@@ -8,11 +8,33 @@ import com.banana.oaback.entity.Employee;
 
 public interface EmployeeDao {
 	
+	/**
+	 * insert an employee record into db 
+	 * @param name
+	 * @param age
+	 * @return 1 if success, otherwise 0
+	 */
 	int insertEmployee(@Param("name") String name, @Param("age") short age);
 	
+	/**
+	 * batch insert employees into db
+	 * @param employees, list of Employee
+	 * @return 1 if success, otherwise 0
+	 */
 	int insertEmployees(List<Employee> employees);
 	
+	/**
+	 * batch update employees
+	 * @param employees, list of Employee need updated
+	 * @return 1 if success, otherwise 0
+	 */
 	int updateEmployees(List<Employee> employees);
 	
+	/**
+	 * query employee records 
+	 * @param offset
+	 * @param limit
+	 * @return list of Employee
+	 */
 	List<Employee> queryAll(@Param("offset") int offset,@Param("limit") int limit);
 }
